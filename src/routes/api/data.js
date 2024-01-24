4ab53e0757040d3e3c34f80e2524fcb16a113ddf
Thiago Possa <thiagopossa@outlook.com>
2024-01-24 18:26:44 -0300

    Correção do teste de api

--- src/routes/api/data.js
+++ src/routes/api/data.js
@@ -1,6 +1,6 @@
 export const get = (req, res) => {
-  if (req.session.user)
-    res.json({ data: "Olá, " + req.session.user.login + "!" });
+  if (req.session.usuario)
+    res.json({ data: "Olá, " + req.session.usuario.nome_completo + "!" });
   else 
     res.status(401).json({ data: "Você não está logado!" });
 };
--- src/routes/cadastro.js
+++ src/routes/cadastro.js
@@ -1,49 +1,53 @@
-//REFATORAR POR COMPLETO ESSA MERDA
+import bcrypt from "bcrypt";
+import { usuarioSchema } from "../models/schemas/usuarioSchema.js";
+import {
+  inserirUsuario,
+  usuarioExiste,
+  obterCargosUsuario,
+  obterTiposUsuario,
+} from "../models/queries/consultasAutenticacao.js";
 
-import bcrypt from 'bcrypt';
-import { inserirUsuario, usuarioExiste, obterCargosUsuario, obterTiposUsuario } from '../models/queries/consultasAutenticacao.js';
-import { usuarioSchema } from '../models/schemas/usuarioSchema.js'
 
-export async function get(req,res) {
-    const tiposCargos = await obterCargosUsuario();
-    const tiposUsuarios = await obterTiposUsuario();
-    
-    res.render('cadastro.ejs', {tiposUsuarios: tiposUsuarios, tiposCargos: tiposCargos});
+export async function get(req, res) {
+  const tiposCargos = await obterCargosUsuario();
+  const tiposUsuarios = await obterTiposUsuario();
+
+  res.render("cadastro.ejs", { tiposUsuarios: tiposUsuarios, tiposCargos: tiposCargos });
 }
 
-export async function post(req,res) {
-    const usuario = req.body;
+export async function post(req, res) {
+  const usuario = req.body;
 
-    try {
-        // Validar os dados do usuário
-        await usuarioSchema.validate(usuario);
+  try {
+    // Validar os dados do usuário
+    await usuarioSchema.validate(usuario);
 
-        // Verificar se o usuário já existe no banco de dados
-        const usuarioExistente = await usuarioExiste(usuario.login_sms);
+    // Verificar se o usuário já existe no banco de dados
+    const usuarioExistente = await usuarioExiste(usuario.login_sms);
 
-        if (usuarioExistente) {
-            // Usuário já existe, redirecione para uma página de erro ou trate conforme necessário
-            res.redirect('/error');
-            return;
-        }
-
-        // Hash da senha antes de salvar no banco de dados
-        const hash = await bcrypt.hash(usuario.senha, 10);
-        
-        usuario.senha = hash;
-        // Insere o usuário no banco de dados
-        const userId = await inserirUsuario(usuario);        
-        // Se a inserção for bem-sucedida, redirecione para a página de dashboard
-        res.redirect('/login');
-    } catch (error) {
-        // Trate os erros conforme necessário
-        console.error(error);
-        res.redirect('/error');
+    if (usuarioExistente) {
+      // Usuário já existe, redirecione para uma página de erro ou trate conforme necessário
+      res.redirect("/error");
+      return;
     }
-};
+
+    // Hash da senha antes de salvar no banco de dados
+    const hash = await bcrypt.hash(usuario.senha, 10);
+
+    usuario.senha = hash;
+    // Insere o usuário no banco de dados
+    const userId = await inserirUsuario(usuario);
+    // Se a inserção for bem-sucedida, redirecione para a página de dashboard
+    res.redirect("/login");
+  } catch (error) {
+    // Trate os erros conforme necessário
+    console.error(error);
+    res.redirect("/error");
+  }
+}
 
 // Função para verificar se o usuário já existe no banco de dados
 async function checkUserExists(login) {
-    const exists = await usuarioExiste(login);
-    return exists;
+  const exists = await usuarioExiste(login);
+  return exists;
 }
